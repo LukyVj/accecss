21208883b103e2e4a644981684299b25b34faf88
Lucas Bonomi <lucas.bonomi@gmail.com>
2020-02-05 14:30:22 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -39,42 +39,4 @@ function deactiveButton (elem) {
 
 var apiURL = 'https://api.github.com/repos/LukyVj/accecss/commits?per_page=6&sha='
 
-var demo = new Vue({
 
-  el: '#commits',
-
- data: {
-    branches: ['gh-pages'],
-    currentBranch: 'gh-pages',
-    commits: null
-  },
-
-  created: function () {
-    this.fetchData()
-    this.$watch('currentBranch', function () {
-      this.fetchData()
-    })
-  },
-
-  filters: {
-    truncate: function (v) {
-      var newline = v.indexOf('\n')
-      return newline > 0 ? v.slice(0, newline) : v
-    },
-    formatDate: function (v) {
-      return v.replace(/T|Z/g, ' ')
-    }
-  },
-
-  methods: {
-    fetchData: function () {
-      var xhr = new XMLHttpRequest()
-      var self = this
-      xhr.open('GET', apiURL + self.currentBranch + '&client_id=769085192c10d1669bee&client_secret=36220dd0aaff915490f4c7b3b1aa70ce4a0bac3f')
-      xhr.onload = function () {
-        self.commits = JSON.parse(xhr.responseText)
-      }
-      xhr.send()
-    }
-  }
-})
